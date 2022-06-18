3de3f38519e0742017063db4e4e12b8399cc7b3f
Aaron Stern <sternaarond@gmail.com>
2022-06-19 00:58:40 +0700

    installed axios for fetching data from API, added useState and useEffect hooks to fetch the Id of public Repos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import RepoList from "./components/RepoList";
+import axios from "axios";
 
 export default function App() {
-  return <div>App</div>;
+  const [repo, setRepo] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://api.github.com/repositories").then((res) => {
+      setRepo(res.data.map((repo) => repo.id));
+    });
+  }, []);
+
+  return (
+    <>
+      <RepoList repo={repo} />
+    </>
+  );
 }
